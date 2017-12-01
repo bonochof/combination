c89fe40722eced143db54f23824c96e9ec3d8f36
bonochof <ryos427@yahoo.co.jp>
2017-12-02 00:06:18 +0900

    クラスの定義とサンプルを追加

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
 # coding utf-8
+
+class Combination
+  def calc( n, r )
+    return 1 if n == r or r == 0
+    return self.calc( n - 1, r - 1 ) + self.calc( n - 1, r )
+  end
+end
+
+hoge = Combination.new
+p hoge.calc(4, 1)
+p hoge.calc(4, 2)
+p hoge.calc(4, 3)
+p hoge.calc(4, 4)
+
